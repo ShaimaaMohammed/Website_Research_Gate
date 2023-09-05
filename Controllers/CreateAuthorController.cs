f2142a96107debe9a25a4d7a84dda4c8940c67a4
ShaimaaMohammed <shaimaam3k@gmail.com>
2023-09-05 22:27:39 +0300

    Research gate

---
+++ Controllers/CreateAuthorController.cs
@@ -0,0 +1,35 @@
+﻿using Research_Gate_1._1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Research_Gate_1._1.Controllers
+{
+    public class CreateAuthorController : Controller
+    {
+        // GET: CreateAuthor
+        [HttpGet]
+        public ActionResult createAuthor()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddAuthor([Bind(Exclude = "ProfileImage")]Author author, HttpPostedFileBase ProfileImage)
+        {
+            ResearchGateEntities context = new ResearchGateEntities();
+            byte[] imgBinaryData = new byte[ProfileImage.ContentLength];
+            int readresult = ProfileImage.InputStream.Read(imgBinaryData, 0, ProfileImage.ContentLength);
+            author.ProfileImage = imgBinaryData;
+            context.Authors.Add(author);
+            context.SaveChanges();
+            var users = context.Authors.ToList();
+            int id = users[users.Count() - 1].ID;
+            Session["userid"] = Convert.ToString(id);
+            return RedirectToAction("Author", "Profile", new { id });
+
+        }
+    }
+}
---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,20 @@
+﻿using Research_Gate_1._1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Research_Gate_1._1.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult Author(int id)
+        {
+            var context = new ResearchGateEntities();
+            var author = context.Authors.Find(id);
+            return View(author);
+        }
+    }
+}
